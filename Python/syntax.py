c337cc253fa4677040d9b44a3ddb13b092b635a8
Ludvig Ek <ekludvig92@gmail.com>
2023-12-31 00:20:42 +0100

    add

---
+++ Python/syntax.py
@@ -0,0 +1,30 @@
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5 #int
+y = "Hello World" #str
+print(x)
+print(y)
+    
+a = str(3)
+b = int(3)
+c = float(3)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+q, w, e = "Orange", "Banana", "Cherry"
+print(q)
+print(w)
+print(e)
+
+r = t = u = "Orange"
+
+print(r)
+print(t)
+print(u)
